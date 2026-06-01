4d8cda9c2aa80ec15deb523eccdbecda3484e05d
Manish Verma <manishverma0757@gmail.com>
2026-06-01 21:38:42 +0530

    Day-3 uplodaded.

---
+++ Day-3/Q10_primenumbersinarange.c
@@ -0,0 +1,33 @@
+//Write a program to Print prime numbers in a range.
+#include <stdio.h>
+
+int main() {
+    int n1, n2, i, j, isPrime;
+
+    printf("Enter the starting number: ");
+    scanf("%d", &n1);
+
+    printf("Enter the ending number: ");
+    scanf("%d", &n2);
+
+    printf("Prime numbers between %d and %d are:\n", n1, n2);
+
+    for (i = n1; i <= n2; i++) {
+        if (i <= 1)
+            continue;
+
+        isPrime = 1;
+
+        for (j = 2; j * j <= i; j++) {
+            if (i % j == 0) {
+                isPrime = 0;
+                break;
+            }
+        }
+
+        if (isPrime)
+            printf("%d ", i);
+    }
+
+    return 0;
+}
---
+++ Day-3/Q9_Checknumberisprime.c
@@ -0,0 +1,27 @@
+//Write a program to Check whether a number is prime.
+
+#include <stdio.h>
+int main() {
+    int num, i, isPrime = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num <= 1) {
+        isPrime = 0;
+    } else {
+        for (i = 2; i < num; i++) {
+            if (num % i == 0) {
+                isPrime = 0;
+                break;
+            }
+        }
+    }
+
+    if (isPrime)
+        printf("%d is a Prime Number\n", num);
+    else
+        printf("%d is not a Prime Number\n", num);
+
+    return 0;
+}
