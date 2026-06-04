98c326624d2b0beabc932f184404e05e9b04f75f
Manish Verma <manishverma0757@gmail.com>
2026-06-04 22:59:47 +0530

    Day-6 uploaded

---
+++ Day-6/Q22_binarytodecimal.c
@@ -0,0 +1,21 @@
+//Write a program to Convert binary to decimal. 
+
+#include <stdio.h>
+
+int main() {
+    int binary, decimal = 0, base = 1, rem;
+
+    printf("Enter a binary number: ");
+    scanf("%d", &binary);
+
+    while (binary > 0) {
+        rem = binary % 10;      // Extract last digit
+        decimal += rem * base;
+        base *= 2;
+        binary /= 10;
+    }
+
+    printf("Decimal = %d\n", decimal);
+
+    return 0;
+}
