967e78d9c80432268cfafd7048b952a230014f3c
Manish Verma <manishverma0757@gmail.com>
2026-06-06 21:58:56 +0530

    Day-8 uploaded

---
+++ Day-8/Q31_charactertriangle.c
@@ -0,0 +1,19 @@
+//Write a program to Print character triangle.
+
+#include <stdio.h>
+
+int main() {
+    int rows;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+
+    for (int i = 1; i <= rows; i++) {
+        for (char ch = 'A'; ch < 'A' + i; ch++) {
+            printf("%c ", ch);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
